7e251ab95b48c34a1a6d6c6c026eb3e7912ff18f
Sergei <serge-koleso@yandex.ru>
2023-12-21 12:02:05 +0300

    modifed Parsing.py

--- ParsingHH.py
+++ ParsingHH.py
@@ -3,18 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 import json
-import re
-#class="serp-item__title"
+
 
 url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
 headers_generator = Headers(os="win", browser="firefox")
 
 response = requests.get(url, headers=headers_generator.generate())
-#main_html_data = response.text
 main_soup = bs4.BeautifulSoup(response.text, 'lxml')
-#vacancies = main_soup.find_all('div', attrs= {'data-qa': 'vacancy-serp__results'})
-#vacancies = main_soup.find_all(class_='vacancy-serp-item__layout')
-#keywords = ['Django', 'Flask']
+
 
 result = []
 for vacancy in main_soup.find_all(class_ = 'vacancy-serp-item__layout'):
@@ -28,20 +24,17 @@ for vacancy in main_soup.find_all(class_ = 'vacancy-serp-item__layout'):
     else:
         salary = salary.text
 
-    desc_item = vacancy.find(class_='vacancy-description-list-item')
-#     desc = desc_item.text if desc_item is not None else ""
-#     if 'django' in (title+desc).lower() or 'flask' in (title+desc).lower():
-    result.append(
-        {
-            'link': link['href'],
-            'company': company,
-            'city': city,
-            'salary': salary,
-        }
-    )
+    desc_item = main_soup.find(class_='g-user-content')
+    desc = desc_item.text if desc_item is not None else ""
+    if "django" in (title + desc).lower() or "flask" in (title + desc).lower():
+        result.append(
+            {
+                'link': link['href'],
+                'company': company,
+                'city': city,
+                'salary': salary,
+            }
+        )
 
 with open("result.json", "w", encoding="utf-8") as file:
     json.dump(result, file, ensure_ascii=False, indent=2)
-
-
-#Не пойму как выбрать вакансии по ключевым словам Django и Flask.
