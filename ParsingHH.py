9d22c452ccc3b38458298424b10f4ef3ac524681
Sergei <serge-koleso@yandex.ru>
2023-12-22 11:47:57 +0300

    Changed Parsing README result

--- ParsingHH.py
+++ ParsingHH.py
@@ -10,31 +10,35 @@ headers_generator = Headers(os="win", browser="firefox")
 
 response = requests.get(url, headers=headers_generator.generate())
 main_soup = bs4.BeautifulSoup(response.text, 'lxml')
-
+vacancy_result = main_soup.find('div', attrs={'data-qa': 'vacancy-serp__results'})
+link = vacancy_result.find_all('div', class_='vacancy-serp-item__layout')
 
 result = []
-for vacancy in main_soup.find_all(class_ = 'vacancy-serp-item__layout'):
-    link = vacancy.find(class_ = 'serp-item__title')
-    title = link.text
-    company = vacancy.find('div', class_ = 'vacancy-serp-item__meta-info-company').text
+for vacancy in link:
+    title_link = vacancy.find('a', class_='serp-item__title')
+    title = title_link['href']
+    title_text = title_link.text
+    company = vacancy.find('div', class_='vacancy-serp-item__meta-info-company').text
     city = vacancy.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-    salary = vacancy.find(class_='bloko-header-section-2')
+    salary = vacancy.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
     if salary is None:
         salary = "Не указана"
     else:
         salary = salary.text
 
-    desc_item = main_soup.find(class_='g-user-content')
-    desc = desc_item.text if desc_item is not None else ""
-    if "django" in (title + desc).lower() or "flask" in (title + desc).lower():
+    response = requests.get(title, headers=headers_generator.generate())
+    vacancy_content = response.text
+    vacancy_soup = bs4.BeautifulSoup(vacancy_content, 'lxml')
+    desc = vacancy_soup.find('div', class_ = 'g-user-content').text
+
+    if "django" in (title_text + desc).lower() or "flask" in (title_text + desc).lower():
         result.append(
             {
-                'link': link['href'],
+                'link': title,
                 'company': company,
                 'city': city,
                 'salary': salary,
             }
         )
-
 with open("result.json", "w", encoding="utf-8") as file:
     json.dump(result, file, ensure_ascii=False, indent=2)
